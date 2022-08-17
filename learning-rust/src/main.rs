aae26fc0c2fd0cfd4595cca6fd36e0abca4bb137
Zsofie <zsofia@linumlabs.com>
2022-08-17 14:52:08 +0200

    basic print commands

--- learning-rust/src/main.rs
+++ learning-rust/src/main.rs
@@ -1,9 +1,21 @@
 fn main() {
     // println is console.log
+
+    //print in next line
     println!("Hello, world!");
     println!("my name is {}","Zsofie");
     println!("I have a {} named {}","cat","Gizmo");
     println!("I'm {} yrs old.",30);
 
+    // print on the same line
+    print!("This is going to");
+    print!("be printed on the same line");
+
+    // named arguments
+    println!("");
+    println!("{name} is a {animal}",  name = "Gizmo",animal = "cat");
+  
+   // basic math
+   println!("25 + 10 = {}",25+10);
 
 }
